b9ac9df340f5b87247d194cf58e615af1d38341d
owen <407953854@qq.com>
2024-04-26 14:47:02 +0800

    更新

--- life/goods/sku_list_test.go
+++ life/goods/sku_list_test.go
@@ -1,19 +1,1 @@
 package goods
-
-//
-//func getCli() *service.Service {
-//	return service.NewService(&oauth.Config{
-//		ClientKey:    "awfu87gyyi5ktef3",
-//		ClientSecret: "953e37e3f8172bf5d9adbfb8730ec123",
-//	}, nil)
-//}
-//
-//func TestSkuList(t *testing.T) {
-//	cli := getCli()
-//	sl, err := SkuList(cli.ClientToken(), "7126518681177163817", "", "", "0")
-//	if err != nil {
-//		t.Error(err)
-//		return
-//	}
-//	fmt.Println(sl)
-//}
